19f87b95271e8539a444a068184bc6b0e4649a0a
= <=>
2023-01-14 16:10:30 +0700

    basic routing + get, post + query parameters

--- src/index.js
+++ src/index.js
@@ -13,11 +13,20 @@ const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
 
+// Dùng Middle Ware
+app.use(express.urlencoded({
+  extended: true
+})) // Xử lý dữ liệu từ FORM gửi lên cho server
+
+app.use(express.json()) // Xử lý dữ liệu từ client gửi lên server (gửi từ dạng javascript lên)
+// VD: XMLHttpRequest, fetch, axios, ajax...
+
+
 // Static File
 app.use(express.static(path.join(__dirname, 'public')))
 
 // HTTP logger: xem thong bao thay doi
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 // Teamplate Engine
 app.engine('.hbs', engine({
@@ -27,11 +36,20 @@ app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname, 'resources/views'))
 
 app.get('/', (req, res) => {
-  res.render('home');
+  res.render('home')
 })
 
 app.get('/news', (req, res) => {
-  res.render('news');
+  res.render('news')
+})
+
+app.get('/search', (req, res) => {
+  res.render('search')
+})
+
+app.post('/search', (req, res) => {
+  // console.log(req.body)
+  res.send('Hello Tin Xun!')
 })
 
 app.listen(port, () => {
